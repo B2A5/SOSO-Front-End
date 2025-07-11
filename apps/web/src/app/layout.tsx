aee6b0e1f68349ea6d78fc6281f7f934d45b5c62
Hwigeon <bb7788@naver.com>
2025-07-12 07:46:52 +0900

    fix: nextjs 방식으로 전역 폰트 프리텐다드 재설정

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,11 +1,13 @@
 // apps/web/app/layout.tsx
-import './globals.css';
+import '@styles/globals.css';
 import type { Metadata } from 'next';
 import { QueryProvider } from '@/providers/queryProvider';
+import localFont from 'next/font/local';
 
 export const metadata: Metadata = {
   title: 'SoSo – Local Biz Helper',
-  description: '지역 주민과 함께 만드는 창업 플랫폼',
+  description:
+    '지역 주민과 함께 만드는 창업 플랫폼',
   viewport: {
     width: 'device-width',
     initialScale: 1,
@@ -14,24 +16,50 @@ export const metadata: Metadata = {
   },
   themeColor: '#4CAF50', //tailwindcss green-500
   //manifest: '/manifest.json',
-  keywords: ['창업', '지역', '소상공인', '아이디어', '투표'],
+  keywords: [
+    '창업',
+    '지역',
+    '소상공인',
+    '아이디어',
+    '투표',
+  ],
   authors: [{ name: 'SOSO Team' }],
-  appleWebApp: { capable: true, statusBarStyle: 'default', title: 'SoSo' },
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: 'SoSo',
+  },
   openGraph: {
     title: 'SoSo – Local Biz Helper',
-    description: '지역 주민과 함께 만드는 창업 플랫폼',
+    description:
+      '지역 주민과 함께 만드는 창업 플랫폼',
     type: 'website',
     locale: 'ko_KR',
   },
 };
+const pretendard = localFont({
+  src: '../public/fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+  variable: '--font-pretendard',
+});
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="ko" className="antialiased">
+    <html
+      lang="ko"
+      className={`antialiased ${pretendard.className}`}
+    >
       <head />
       <body className="flex flex-col h-screen">
         <QueryProvider>
-          <main className="w-full max-w-[640px] mx-auto flex-1 overflow-auto">{children}</main>
+          <main className="w-full max-w-[640px] mx-auto flex-1 overflow-auto">
+            {children}
+          </main>
         </QueryProvider>
       </body>
     </html>
