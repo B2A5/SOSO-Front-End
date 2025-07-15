3b38b54a6d0b17fcc50d8e53e945eb682ff4d403
Hwigeon <bb7788@naver.com>
2025-07-15 19:56:19 +0900

    chore: fix cookies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,14 @@
 // 루트 ESLint configuration
+
 // .eslintrc.cjs
 module.exports = {
   root: true,
+  ignorePatterns: [
+    '.eslintrc.cjs',
+    'apps/docs/**',
+    'apps/docs',
+    '**/apps/docs/**',
+  ],
   extends: ['./tools/eslint-config/index.cjs'],
   plugins: ['@typescript-eslint'],
 };
