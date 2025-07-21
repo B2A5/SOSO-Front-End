088b50990bc2b19a1543c820e8d242c731b7fc59
Hwigeon <bb7788@naver.com>
2025-07-21 21:13:29 +0900

    feat: 메인 페이지 레이아웃 디자인

--- apps/web/src/app/main/community/page.tsx
+++ apps/web/src/app/main/community/page.tsx
@@ -1,7 +1,9 @@
 // apps/web/app/main/community/page.tsx
 import Input from '@/components/inputs/Input';
 import PillButton from './components/PillButton';
-import { Search } from 'lucide-react';
+import Link from 'next/link';
+import { ChevronRight, Search } from 'lucide-react';
+import Card from '@/components/Card';
 /**
  * 커뮤니티 메인 페이지
  *
@@ -19,12 +21,30 @@ export default function CommunityPage() {
           <PillButton className="absolute bottom-10 right-6" />
         </div>
         {/* 커뮤니티 내용 영역 */}
-        <div className="flex-1 w-full max-w-3xl px-4 py-6">
+        <div className="flex-1 w-full gap-4 px-4 pt-6">
           <Input
             placeholder="검색"
             className="mb-4"
             leftIcon={<Search className="h-5 w-5 stroke-current" />}
           />
+          {/* 진행중인 아이디어 이동 */}
+          <div className="flex justify-between items-center ">
+            <h3 className="text-title1">진행중인 아이디어</h3>
+            <Link
+              href="/main/community/votes"
+              className="flex items-center gap-2 hover:text-soso-600 active:text-soso-600"
+            >
+              <ChevronRight />
+            </Link>
+          </div>
+          {/* 아이디어 카드 목록 */}
+          <div className="grid grid-cols-1 gap-4">
+            {/* 아이디어 카드 컴포넌트 */}
+            <Card>
+              <h4 className="text-title2">아이디어 제목</h4>
+              <p className="text-body">아이디어 설명</p>
+            </Card>
+          </div>
         </div>
       </div>
     </div>
