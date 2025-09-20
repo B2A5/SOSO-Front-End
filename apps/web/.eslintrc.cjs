c016e8890dd150e12f87b37ad1859fe3a1145353
Hwigeon <bb7788@naver.com>
2025-09-21 03:24:35 +0900

    fix: ci 워크플로우 수정(타입체크, 테스트 커버리지, 빌드 실행)

---
+++ apps/web/.eslintrc.cjs
@@ -0,0 +1,31 @@
+// apps/web/.eslintrc.js
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  ignorePatterns: ['stylelint.config.js', 'postcss.config.mjs'],
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [
+    {
+      // 이 패턴에 걸리는 파일은 타입 검사(parserOptions.project) 하지 않음
+      files: [
+        'next.config.js',
+        'postcss.config.cjs',
+        'tailwind.config.js',
+        '.eslintrc.js',
+      ],
+      parserOptions: {
+        project: null, // or []
+      },
+    },
+  ],
+  rules: {
+    // 기존 규칙
+  },
+};
