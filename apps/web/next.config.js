79555faeb155e3245a068082bf5d59a732cf198f
Hwigeon <bb7788@naver.com>
2025-09-21 04:57:04 +0900

    feat: bundle Analyzer 설치

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,3 +1,9 @@
+import bundleAnalyzer from '@next/bundle-analyzer';
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
@@ -43,4 +49,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
