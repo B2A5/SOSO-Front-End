549a05fef6530cedbdf4b899b973ff0192b0d5ae
Hwigeon <bb7788@naver.com>
2025-12-06 18:15:52 +0900

    chore: package의 의존성 제어 및 react용 라이브러리 lint를 개선했습니다.

--- packages/eslint-config/library.cjs
+++ packages/eslint-config/library.cjs
@@ -40,5 +40,11 @@ module.exports = {
     'react/jsx-props-no-spreading': 'off',
     // TypeScript 사용 시 defaultProps 불필요 (optional props 사용)
     'react/require-default-props': 'off',
+    // named export를 선호 (Tree-shaking에 유리)
+    'import/prefer-default-export': 'off',
+    // 화살표 함수 스타일 자유롭게
+    'arrow-body-style': 'off',
+    // useEffect cleanup 함수 등에서 유연한 return 허용
+    'consistent-return': 'off',
   },
 };
