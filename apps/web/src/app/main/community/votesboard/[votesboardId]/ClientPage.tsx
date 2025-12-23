864317b40c9c868dd5923cf154eafd7ad43f001f
Hwigeon <bb7788@naver.com>
2025-12-24 00:26:26 +0900

    feat: 투표 상세 페이지 레이아웃 구현

---
+++ apps/web/src/app/main/community/votesboard/[votesboardId]/ClientPage.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import React from 'react';
+import { Header } from '@/components/header/Header';
+import VoteBoardDetail from './components/VotesBoardDetail';
+export default function VotesBoardDetailClientPage({
+  votesboardId,
+}: {
+  votesboardId: number;
+}) {
+  const menuButtonOnClick = () => {
+    console.log('Menu button clicked');
+    console.log(`Votesboard ID: ${votesboardId}`);
+  };
+
+  return (
+    <main>
+      <Header>
+        <Header.Left>
+          <Header.BackButton />
+        </Header.Left>
+        <Header.Center>투표게시판</Header.Center>
+        <Header.Right>
+          <Header.MenuButton onClick={menuButtonOnClick} />
+        </Header.Right>
+      </Header>
+
+      <section>
+        <VoteBoardDetail votesboardId={votesboardId} />
+      </section>
+      <section>{/* 댓글 섹션 추후 구현 예정 */}</section>
+    </main>
+  );
+}
